af7d63f7d8416ff3af3230a000e1cf112db79247
sagaranatsuki <sagara.n.aa@m.titech.ac.jp>
2019-06-18 16:39:44 +0900

    first

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,16 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int total_bags, int people, int size_bag){
+  int current_bags = 0;
+  int i;
+
+  for (i=0;i<people;i++){
+    current_bags += (A[i] + size_bag - 1)/size_bag;
+  }
+
+  return(current_bags <= total_bags);
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +21,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = A[i-1] + 1;
+  while (ub - lb > 1){
+    int m = (ub + lb) / 2;
+    if (p(k, n, m))ub = m;
+    else lb = m;
+  }
 
-
+  printf("%d\n", ub);
   return 0;
 }
